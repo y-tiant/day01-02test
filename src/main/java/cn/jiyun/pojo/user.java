06c62e8edb16bd876fb7e1d74b285f5912e99bfb
y-tiant <1847946473@qq.com>
2020-04-22 11:32:13 +0800

    提交项目

---
+++ src/main/java/cn/jiyun/pojo/user.java
@@ -0,0 +1,13 @@
+package cn.jiyun.pojo;
+
+public class user {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
